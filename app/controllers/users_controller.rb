afbe37696eb88eea34165ab9844a00ff258bb0d9
Alex Chew <geminia79@gmail.com>
2017-05-25 18:17:39 +0800

    sign_up form tested rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,30 @@
 class UsersController < ApplicationController
 
 
-        def index
+          def index
+          end
+
+          def show
+            @user = User.find(params[:id])
+          end
+
+          def new
+            @user = User.new
+          end
+
+          def create
+            @user = User.new(user_params)
+            if @user.save
+              flash[:success] = 'User created successfully'
+              redirect_to @user
+            else
+              render 'new'
+          end
         end
 
-        def new
-          @user = User.new
-        end
+        private
 
-        def create
-          @user = User.new
-          if @user.save
-            redirect_to root_path
-          else
-            render 'new'
+        def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
         end
       end
-
-    end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#new'
 
   get 'static_pages/help'
+  get 'signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

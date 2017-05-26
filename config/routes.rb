b37f87ca91f4b23c844b91a062a2c4a2c06505c3
Alex Chew <geminia79@gmail.com>
2017-05-26 14:46:15 +0800

    login form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   root 'users#new'
 
   get 'static_pages/help'
   get 'signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

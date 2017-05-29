315256ac3714efb3b8f58e8d358807fce54a89cd
Alex Chew <geminia79@gmail.com>
2017-05-29 12:01:11 +0800

    added perm&forget user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,17 +6,43 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Returns the current logged in user, if there is any
+#remember a user in a permanent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def current_user?(user)
+   user == current_user
+ end
+
+  # Returns the user corresponding to the remember token cookie
   def current_user
-    #@current_user = @current_user || User.find_by(id: session[:user_id])
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      #@current_user = @current_user || User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
     !current_user.nil?
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 #log out the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
